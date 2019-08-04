66921846c2e628dd63dbba5aedb3d61e35833335
AJ Henley <ajhenley@gmail.com>
2019-08-04 09:59:42 -0400

    talking to database

--- src/main/java/com/byaj/mytry/HomeController.java
+++ src/main/java/com/byaj/mytry/HomeController.java
@@ -1,13 +1,33 @@
 package com.byaj.mytry;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+  @Autowired
+  MovieRepository repository;
+
   @RequestMapping("/")
-  public String dude()
-  {
+  public String dude(Model model){
+    model.addAttribute("movies", repository.findAll());
     return "index";
+  }
+
+  @RequestMapping("/add")
+  public String movieForm(Model model){
+    model.addAttribute("movie", new Movie());
+    return "movieform";
+  }
+
+  @RequestMapping("/process")
+  public String processMovie(@ModelAttribute Movie movie)
+  {
+    repository.save(movie);
+    return "redirect:/";
   }
 }
